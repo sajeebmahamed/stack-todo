77cf663fb2c83b2bbb0b3bd6c91bfabff3f1549a
Sajeeb <sajeebahamed6@gmail.com>
2021-02-02 16:18:09 +0600

    stack todo done

--- src/components/listview/index.jsx
+++ src/components/listview/index.jsx
@@ -1,56 +1,57 @@
-import PropsTypes from 'prop-types'
-import React from 'react'
-import { Button, CustomInput, ListGroup, ListGroupItem } from 'reactstrap'
+import PropTypes from 'prop-types';
+import React from 'react';
+import { Button, CustomInput, ListGroup, ListGroupItem } from 'reactstrap';
 
+// List Item Component
 const ListItem = ({ todo, toggleSelect, toggleComplete }) => {
-    return (
-        <ListGroupItem className='d-flex align-items-center'>
-            <CustomInput
-                type='checkbox'
-                id={todo.id}
-                checked={todo.isSelect}
-                onChange={ () => toggleSelect(todo.id) }
-            />
-            <div className='mx-3'>
-                <h4> { todo.text } </h4>
-                <p> {todo.time.toDateString()} </p>
-            </div>
-            <Button
-                className='ml-auto'
-                color={todo.isComplete ? 'danger' : 'success'}
-            >
-                { todo.isComplete ? 'Complete' : 'Running' }
-            </Button>
-        </ListGroupItem>
-    )
-}
+	return (
+		<ListGroupItem className='d-flex align-items-center'>
+			<CustomInput
+				type='checkbox'
+				id={todo.id}
+				checked={todo.isSelect}
+				onChange={() => toggleSelect(todo.id)}
+			/>
+			<div className='mx-3'>
+				<h4>{todo.text}</h4>
+				<p>{todo.time.toDateString()}</p>
+			</div>
+			<Button
+				className='ml-auto'
+				color={todo.isComplete ? 'danger' : 'success'}
+				onClick={() => toggleComplete(todo.id)}
+			>
+				{todo.isComplete ? 'Completed' : 'Running'}
+			</Button>
+		</ListGroupItem>
+	);
+};
 
-ListItem.prototype = {
-    todo: PropsTypes.object.isRequired,
-    toggleSelect: PropsTypes.func.isRequired,
-    toggleComplete: PropsTypes.func.isRequired
-}
+ListItem.propTypes = {
+	todo: PropTypes.object.isRequired,
+	toggleSelect: PropTypes.func.isRequired,
+	toggleComplete: PropTypes.func.isRequired
+};
 
 const ListView = ({ todos, toggleSelect, toggleComplete }) => {
-    return(
-        <ListGroup>
-            {todos.map(todo => (
-                <ListItem
-                    key={todo.id}
-                    todo={todo}
-                    toggleSelect={toggleComplete}
-                    toggleComplete={toggleComplete}
-                />
-            ))
+	return (
+		<ListGroup>
+			{todos.map(todo => (
+				<ListItem
+					key={todo.id}
+					todo={todo}
+					toggleSelect={toggleSelect}
+					toggleComplete={toggleComplete}
+				/>
+			))}
+		</ListGroup>
+	);
+};
 
-            }
-        </ListGroup>
-    )
-}
-ListGroup.prototype = {
-    todos: PropsTypes.object.isRequired,
-    toggleSelect: PropsTypes.func.isRequired,
-    toggleComplete: PropsTypes.func.isRequired
-}
+ListView.propTypes = {
+	todos: PropTypes.object.isRequired,
+	toggleSelect: PropTypes.func.isRequired,
+	toggleComplete: PropTypes.func.isRequired
+};
 
-export default ListView
+export default ListView;
--- src/components/todos/index.jsx
+++ src/components/todos/index.jsx
@@ -1,50 +1,179 @@
-import React from 'react'
-import ListView from '../listview'
-import TableView from '../tableview'
+import React from 'react';
+import { Modal, ModalBody, ModalHeader } from 'reactstrap';
+import shortid from 'shortid';
+import Controller from '../controllers';
+import CreateTodoForm from '../create-todo-form';
+import ListView from '../listview';
+import TableView from '../tableview';
+
 
 class Todos extends React.Component {
-    state = {
-        todos: [
-            {
-                id: 1,
-                text: 'Lorem Ipsum',
-                description: 'Lorem description',
-                time: new Date(),
-                isComplete: false,
-                isSelect: false
-            },
-            {
-                id: 2,
-                text: 'Lorem Ipsum2',
-                description: 'Lorem description2',
-                time: new Date(),
-                isComplete: false,
-                isSelect: false
-            }
-        ]
-    }
-    toggleSelect = todoId => {}
-    toggleComplete = todoId => {}
-    render() {
-        return (
-            <div>
-                <h1 className='display-4 text-center mb-5'> todos </h1>
-                <div>
-                    <ListView
-                        todos={this.state.todos}
-                        toggleSelect={this.toggleSelect}
-                        toggleComplete={this.toggleComplete}
-                    />
-                </div>
-                <div>
-                    <TableView
-                        todos={this.state.todos}
-                        toggleSelect={this.toggleSelect}
-                        toggleComplete={this.toggleComplete}
-                    />
-                </div>
-            </div>
-        )
-    }
+	state = {
+		todos: [
+			{
+				id: 'dsfsds3fds',
+				text: 'main todo text',
+				description: 'simple description',
+				time: new Date(),
+				isComplete: false,
+				isSelect: false
+			},
+			{
+				id: 'dsfsdfdsdsfs',
+				text: 'main Another Task',
+				description: 'simple description',
+				time: new Date(),
+				isComplete: false,
+				isSelect: false
+			},
+			{
+				id: 'dsfsdfdsdsdsfs',
+				text: 'New Task',
+				description: 'simple description',
+				time: new Date(),
+				isComplete: false,
+				isSelect: false
+			}
+		],
+		isOpenTodoForm: false,
+		searchTerm: '',
+		view: 'list',
+		filter: 'all'
+	};
+
+	toggleSelect = todoId => {
+		const todos = [...this.state.todos];
+		const todo = todos.find(t => t.id === todoId);
+		todo.isSelect = !todo.isSelect;
+
+		this.setState({ todos });
+	};
+
+	toggleComplete = todoId => {
+		const todos = [...this.state.todos];
+		const todo = todos.find(t => t.id === todoId);
+		todo.isComplete = !todo.isComplete;
+
+		this.setState({ todos });
+	};
+
+	toggleForm = () => {
+		this.setState({
+			isOpenTodoForm: !this.state.isOpenTodoForm
+		});
+	};
+
+	handleSearch = value => {
+		this.setState({ searchTerm: value });
+	};
+
+	createTodo = todo => {
+		todo.id = shortid.generate();
+		todo.time = new Date();
+		todo.isComplete = false;
+		todo.isSelect = false;
+
+		const todos = [todo, ...this.state.todos];
+		this.setState({ todos });
+		this.toggleForm();
+	};
+
+	handleFilter = filter => {
+		this.setState({ filter });
+	};
+
+	changeView = event => {
+		this.setState({
+			view: event.target.value
+		});
+	};
+
+	clearSelected = () => {
+		const todos = this.state.todos.filter(todo => !todo.isSelect);
+		this.setState({ todos });
+	};
+
+	clearCompleted = () => {
+		const todos = this.state.todos.filter(todo => !todo.isComplete);
+		this.setState({ todos });
+	};
+
+	reset = () => {
+		this.setState({
+			filter: 'all',
+			searchTerm: '',
+			view: 'list',
+			isOpenTodoForm: false
+		});
+	};
+
+	performSearch = () => {
+		return this.state.todos.filter(todo =>
+			todo.text
+				.toLowerCase()
+				.includes(this.state.searchTerm.toLowerCase())
+		);
+	};
+
+	performFilter = todos => {
+		const { filter } = this.state;
+		if (filter === 'completed') {
+			return todos.filter(todo => todo.isComplete);
+		} else if (filter === 'running') {
+			return todos.filter(todo => !todo.isComplete);
+		} else {
+			return todos;
+		}
+	};
+
+	getView = () => {
+		let todos = this.performSearch();
+		todos = this.performFilter(todos);
+		return this.state.view === 'list' ? (
+			<ListView
+				todos={todos}
+				toggleSelect={this.toggleSelect}
+				toggleComplete={this.toggleComplete}
+			/>
+		) : (
+			<TableView
+				todos={todos}
+				toggleSelect={this.toggleSelect}
+				toggleComplete={this.toggleComplete}
+			/>
+		);
+	};
+
+	render() {
+		return (
+			<div>
+				<h1 className='display-4 text-center mb-5'>Stack Todos</h1>
+				<Controller
+					term={this.state.searchTerm}
+					view={this.state.view}
+					toggleForm={this.toggleForm}
+					handleSearch={this.handleSearch}
+					handleFilter={this.handleFilter}
+					changeView={this.changeView}
+					clearSelected={this.clearSelected}
+					clearCompleted={this.clearCompleted}
+					reset={this.reset}
+				/>
+				<div>{this.getView()}</div>
+				<Modal
+					isOpen={this.state.isOpenTodoForm}
+					toggle={this.toggleForm}
+				>
+					<ModalHeader toggle={this.toggleForm}>
+						Create New Todo Item
+					</ModalHeader>
+					<ModalBody>
+						<CreateTodoForm createTodo={this.createTodo} />
+					</ModalBody>
+				</Modal>
+			</div>
+		);
+	}
 }
-export default Todos
+
+export default Todos;
